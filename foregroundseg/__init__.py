6b1d49ae241899aa924c67cbc28e16a176a78e5b
zymolyase-hub <sven.heckwolf@gmail.com>
2026-07-08 17:20:57 +0200

    Update __init__.py

--- foregroundseg/__init__.py
+++ foregroundseg/__init__.py
@@ -1,1 +1,14 @@
+"""
+ForegroundSeg
+==============
 
+AI-powered foreground/background segmentation using PyTorch.
+"""
+
+from .version import __version__
+from .predictor import ForegroundPredictor
+
+__all__ = [
+    "ForegroundPredictor",
+    "__version__",
+]
