a5b0b0f86e4c9067cd9ae217672f57e01f488d3d
zymolyase-hub <sven.heckwolf@gmail.com>
2026-07-08 17:21:43 +0200

    Update config.py

--- foregroundseg/config.py
+++ foregroundseg/config.py
@@ -1,1 +1,12 @@
+from pathlib import Path
 
+PACKAGE_ROOT = Path(__file__).parent
+PROJECT_ROOT = PACKAGE_ROOT.parent
+
+MODEL_NAME = "DeepLabV3"
+
+DEFAULT_DEVICE = "cuda"
+
+EXAMPLES_DIR = PROJECT_ROOT / "examples"
+
+OUTPUT_DIR = PROJECT_ROOT / "output"
